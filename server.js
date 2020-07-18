b1456785a46cf364c93edd7b8261fee2103ce9db
rahulmhjn <rahul.mahajan676@gmail.com>
2020-07-19 03:41:11 +0530

    some changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 let broadcaster;
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const http = require("http");
 const server = http.createServer(app);
